9a291d55c72ee10ad28feed706c4bc398d0d431a
vboxuser <tspartner21@gmail.com>
2024-09-19 15:43:33 +0900

    change/trim함수

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
-//전체 문자열에서 특정 문자값을 기준으로 분리해서 반환하는 내장함수
-const span_text = document.querySelector('span').innerText;
-console.log(span_text); //2024-09-19
+function chagneText(elem, spc1, spc2) {
+  const e1 = document.querySelector(elem);
 
-const text_arr = span_text.split('-');
-console.log(text_arr);//['2024','09','19'];
-const new_text = text_arr.join('.');
-console.log(new_text); //2024.09.19
+  const e1_text = e1.innerText.trim();
 
-//위와 split과 join을 한번에 같이 map을 활용해서 연산 간소화하는 방법
-const h2_text = document.querySelector("h2").innerText;
-const new_h2 = h2_text.split(" ").join("/");
-console.log(new_h2);
-
-//미션 - 위의 구문을 다음과 같은 형태의 함수로 패키징
-//changeText('h2', ' ', '/');
-
-function changeText(selector, target, change) {
-  const text = document.querySelector(selector).innerText;
-  const new_text = text.split(target).join(change);
-  console.log(new_text);
+  e1.innerText = e1_text.split(spc1).join(spc2);
 }
 
-changeText('h2', ' ', ' # ');
+// chagneText("h1", "-", ".");
+// chagneText("h2", " ", "/");
+// chagneText("h3", "_", "+");
+
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+  chagneText("h1", "-", ".");
+  chagneText("h2", " ", "/");
+  chagneText("h3", "_", " ");
+});
