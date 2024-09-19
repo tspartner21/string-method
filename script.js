f274a6449e0676e74e85e689231954a95fcce44f
vboxuser <tspartner21@gmail.com>
2024-09-19 15:26:54 +0900

    chageText함수

--- script.js
+++ script.js
@@ -1,50 +1,24 @@
-console.log("test");
-const h1 = document.querySelector("h1");
-h1.innerText = "Test";
+//전체 문자열에서 특정 문자값을 기준으로 분리해서 반환하는 내장함수
+const span_text = document.querySelector('span').innerText;
+console.log(span_text); //2024-09-19
 
-const h2 = document.querySelector("h2");
-const linkURL = "https://wwww.naver.com";
-h2.innerHTML = `<a href=${linkURL}>naver</a>`;
+const text_arr = span_text.split('-');
+console.log(text_arr);//['2024','09','19'];
+const new_text = text_arr.join('.');
+console.log(new_text); //2024.09.19
 
-const h3 = document.querySelector("h3");
-const h3_text = h3.innerText;
-//h3 문자열을 0위치에서부터 50번재 위치까지 짜른뒤
-//짜른 문자열을 다시 h3.innerText 프로퍼티에 대입해서 적용
+//위와 split과 join을 한번에 같이 map을 활용해서 연산 간소화하는 방법
+const h2_text = document.querySelector("h2").innerText;
+const new_h2 = h2_text.split(" ").join("/");
+console.log(new_h2);
 
+//미션 - 위의 구문을 다음과 같은 형태의 함수로 패키징
+//changeText('h2', ' ', '/');
 
-//미션 - 조건문을 활용해서 h3에 들어가는 문자값이 50글자가 안되면
-//해당 문자를 그대로 출력하고 50글자가 넘어가면
-//50글자까지 자른뒤 그뒤에 ... 말줄임표를 붙이기
-
-// console.log(h3.innerText.length);
-
-
-// if (h3_text.length > 50) {
-//   h3_Textcheck = h3_text.substring(0, 50);
-//   h3.innerText = `${h3_Textcheck} ...`;
-// }
-// else {
-//   h3.innerText = h3_text;
-// }
-
-// if (h3_text.length > 30) h3.innerText = h3_text.substring(0, 30) + "...";
-// else h3.innerText = h3_text;
-//삼항연산자로 변경
-// h3.innerText = h3_text.length > 30 ? h3_text.substring(0, 30) + "..." : h3_text;
-
-//미션 - 위의 기능을 재사용하기 편하도록 함수로 패키징
-//shortenText('h3' , 30);
-
-function shortenText(querySelector, maxlength) {
-  //첫 번째 인수로 받은 문자값으로 DOM요소 선택
-  const element = document.querySelector(querySelector);
-  //해당 요소의 문자값 변수에 할당
-  const elment_text = element.innerText;
-
-  //해당 돔요소의 innerText 프로퍼티에 대입되는 우항을 삼항연수로 변경
-  element.innerText = elment_text.length > maxlength ? elment_text.substring(0, maxlength) + "..." : elment_text;
+function changeText(selector, target, change) {
+  const text = document.querySelector(selector).innerText;
+  const new_text = text.split(target).join(change);
+  console.log(new_text);
 }
 
-shortenText('h1', 2);
-shortenText('h2', 2);
-shortenText('h3', 20);
+changeText('h2', ' ', ' # ');
